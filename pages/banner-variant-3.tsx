215468bbcbb009824af86afa9361db6273faf07d
Mukesh Prajapti <mukesh.prajapati@easternts.com>
2023-07-26 10:40:12 +0530

    BG Color Variant separated

--- pages/banner-variant-3.tsx
+++ pages/banner-variant-3.tsx
@@ -1,14 +1,14 @@
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import ToggleBanner from "../src/ToggleBanner";
-import { diffBGBannerData } from "../src/data/banner-data";
+import { bannerData } from "../src/data/banner-data";
 import StyledFullBleed from "../src/data/StyledFullBleed.styled";
 import theme from "../src/theme";
 
 export default function BannerVariant3() {
   return (
     <StyledFullBleed theme={theme} className="full-bleed">
-      <ToggleBanner data={diffBGBannerData}></ToggleBanner>;
+      <ToggleBanner data={bannerData}></ToggleBanner>;
     </StyledFullBleed>
   );
 }
